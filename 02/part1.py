21ccfb81e85b69072d35ed95688af71ea7c7a945
Oliver <oliver.gruendel2004@gmail.com>
2022-12-02 14:58:51 +0100

    Day 2

--- 02/part1.py
+++ 02/part1.py
@@ -29,4 +29,35 @@ for line in game:
             # Paper
             score += 6
 
+game.close()
 print(f'Part 1 solution: {score}')
+score = 0
+game = open("input", "r")
+
+for line in game:
+    if line[0] == "A":
+        # Rock
+        if line[2] == "X":
+            score += 3
+        elif line[2] == "Y":
+            score += 4
+        elif line[2] == "Z":
+            score += 8
+    elif line[0] == "B":
+        # Paper
+        if line[2] == "X":
+            score += 1
+        elif line[2] == "Y":
+            score += 5
+        elif line[2] == "Z":
+            score += 9
+    elif line[0] == "C":
+        # Scissors
+        if line[2] == "X":
+            score += 2
+        elif line[2] == "Y":
+            score += 6
+        elif line[2] == "Z":
+            score += 7
+
+print(f'Part 2 solution: {score}')
