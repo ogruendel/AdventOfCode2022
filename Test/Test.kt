ea18e443d3eb7646d5444e486747776eeaafec8c
Oliver <oliver.gruendel2004@gmail.com>
2022-12-12 18:57:25 +0100

    Day 10 Testing

--- Test/Test.kt
+++ Test/Test.kt
@@ -1,11 +1,5 @@
 package Test
 
 fun main() {
-    val test: MutableMap<String, Int> = mutableMapOf("a" to 123)
-    if (test["b"] == null) {
-        test["b"] = 0
-    }
-    test["b"] = test.getValue("b") + 1
-    println(test.getValue("b"))
-
+    println(0 in 1..3)
 }
